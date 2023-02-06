fe9f0e69ffdbda250a23dfe3e6e6e9b09c08b1dc
rafisovic4 <Oskar_1223@mail.ru>
2023-02-06 13:05:11 +0300

    Add: slider and preloader

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,17 @@
-import { NavLink } from "react-router-dom";
+  
 import {
-  ArrowNextIcon,
-  ArrowPrevIcon,
   SearchIcon,
 } from "../components/Icons/Icons";
 
 import product from "../assets/product_1.png";
 
-import banner from "../assets/banner.png";
 import Product from "../components/Product/Product";
+import Slider from "../components/Slider/Slider";
 
 const HomePage = () => {
   return (
     <>
-      <section className="slider">
-        <div className="wrapper">
-          <div className="slides">
-            <div
-              className="slide"
-              style={{
-                backgroundColor: "#F4EFE9",
-              }}
-            >
-              <div className="slide__left">
-                <h2>Stan Smith, Forever!</h2>
-                <NavLink className="button xl" to="/">
-                  Купить
-                </NavLink>
-              </div>
-              <div className="slide__right">
-                <img src={banner} alt="Stan Smith" />
-              </div>
-            </div>
-          </div>
-          <div className="control">
-            <button className="prev">
-              <ArrowPrevIcon />
-            </button>
-            <button className="next">
-              <ArrowNextIcon />
-            </button>
-          </div>
-        </div>
-      </section>
+      <Slider />
       <section className="section catalog">
         <header className="section-header">
           <h2 className="section__title">Все кроссовки</h2>
